171fec9d9115c39855d77280b7d27524d0c4fc84
Eli B. <eli.blazek20@gmail.com>
2025-07-09 11:18:36 +0200

    Did another complete rewrite. Half done. Good code.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,15 +1,19 @@
 import os
 
-# The directory parameter should be treated as a relative path within the working_directory
-def get_files_info(working_directory, directory=None):
-    wd_contents = os.listdir(working_directory)
-    full_path = os.path.join(working_directory, directory)
+def get_files_info(working_directory, directory = None):
+    ### Pre-check
+    wd_path_check = os.path.normpath(os.path.abspath(working_directory))
+    cd_path_check = os.path.normpath(os.path.abspath(working_directory))
     
-    if directory not in wd_contents:
+    ### Prep path to reasonable format
+    if directory is not None:
+        full_path_wd = os.path.abspath(os.path.join(working_directory, directory))
+    else:
+        full_path_wd = os.path.abspath(working_directory)
+    full_path_wd = os.path.normpath(full_path_wd)
+    print(full_path_wd)
+    ### Check if dir is part of workdir and is not a file
+    if not full_path_wd.startswith(wd_path_check):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-    if not (os.path.isdir(directory):
+    elif os.path.isdir(cd_path_check):
         return f'Error: "{directory}" is not a directory'
-    
-
-    
--- tests.py
+++ tests.py
@@ -1,7 +1,18 @@
 # from subdirectory.filename import function_name
 from functions.get_files_info import get_files_info
 
+print("Case 1:\n---------------------------")
 print(get_files_info("calculator", ".")) # Should pass
+print("Should pass")
+print("Case 2:\n---------------------------")
 print(get_files_info("calculator", "pkg")) # Should pass
+print("Should pass")
+print("Case 3:\n---------------------------")
 print(get_files_info("calculator", "/bin")) # Should print err string
+print("Should fail")
+print("Case 4:\n---------------------------")
 print(get_files_info("calculator", "../")) # Should print err string
+print("Should fail")
+print("Case 5:\n---------------------------")
+print(get_files_info("calculator", "tests.py")) # Should print err string
+print("Should fail")
